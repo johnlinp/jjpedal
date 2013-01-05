91771c687b51f29bdc851869164e2d194ba5ca46
averangeall <averangeall@gmail.com>
2013-01-05 20:03:22 +0800

    Finish basic argument parsing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import alsaaudio
 
 def init_pcm(pcm):
@@ -6,7 +7,9 @@ def init_pcm(pcm):
     pcm.setformat(alsaaudio.PCM_FORMAT_S16_LE)
     pcm.setperiodsize(160)
 
-def main():
+def play_on_the_fly():
+    print 'now play your guitar!'
+
     inputt = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK)
     output = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK)
 
@@ -18,6 +21,61 @@ def main():
         if l:
             output.write(data)
 
+def watch_histogram(src_fname):
+    print 'watch histogram of ' + src_fname
+
+def play_file(src_fname):
+    print 'play ' + src_fname
+
+def record_file(dst_fname):
+    print 'record ' + dst_fname
+
+def print_usage():
+    print 'usage:'
+    print '    . playing the guitar on the fly'
+    print '        python main.py'
+    print '    . recording the guitar sound'
+    print '        python main.py -o filename.jj'
+    print '    . playing the guitar sound'
+    print '        python main.py -i filename.jj'
+    print '    . watching the histogram'
+    print '        python main.py -i filename.jj -h'
+    exit()
+
+def main(argv):
+    src_fname = None
+    dst_fname = None
+    histogram = False
+    idx = 1
+    while idx < len(argv):
+        if argv[idx] == '-i':
+            if idx >= len(argv) - 1:
+                print_usage()
+            src_fname = argv[idx + 1]
+            idx += 1
+        elif argv[idx] == '-o':
+            if idx >= len(argv) - 1:
+                print_usage()
+            dst_fname = argv[idx + 1]
+            idx += 1
+        elif argv[idx] == '-h':
+            histogram = True
+        else:
+            print_usage()
+        idx += 1
+
+    if src_fname != None and dst_fname == None:
+        if histogram:
+            watch_histogram(src_fname)
+        else:
+            play_file(src_fname)
+    elif src_fname == None and dst_fname != None and not histogram:
+        record_file(dst_fname)
+    elif src_fname == None and dst_fname == None and not histogram:
+        play_on_the_fly()
+    else:
+        print_usage()
+
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
 
